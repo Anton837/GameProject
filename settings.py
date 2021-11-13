7d58dd3cd036c35baf71c19867511f654cca92aa
Anton837 <62432374+Anton837@users.noreply.github.com>
2021-11-14 01:03:21 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,17 @@
 WIDTH = 700
 HEIGHT = 900
+menu_width = 200
+draw_width = WIDTH - menu_width
+draw_height = HEIGHT
 FPS = 30
-TILE = 25
+TILE = 20
+
+colors = {
+    "red": (255, 0, 0),
+    "blue": (0, 255, 0),
+    "green": (0, 0, 255),
+    "orange": (255, 165, 0),
+    "yellow": (255, 255, 0),
+    "darkgreen": (0, 100, 0),
+    "pink": (255, 192, 203)
+}
